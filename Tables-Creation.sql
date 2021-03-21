1311afa0e2b2b91ccaf3d54e318e02534176680d
Jonas Blanc <jonas.blanc@epfl.ch>
2021-03-21 16:04:39 +0100

    Add everything to the left of collision

--- Tables-Creation.sql
+++ Tables-Creation.sql
@@ -14,6 +14,13 @@
 --in Victims: attribute victim_seating_position_id || seating_position_id
 -- merge state: Unknown with blank ? => key == null ?
 
+-- Used in both victimd and parties
+CREATE TABLE Safety_equipment
+(
+    id char(1) not null,
+    definition varchar(150),
+    PRIMARY KEY (id)
+);
 
 ------------------------------------------------------------------------
 ------------------------------------------------------------------------
@@ -60,6 +67,13 @@ CREATE TABLE Victim_ejected
     PRIMARY KEY (id)
 );
 
+CREATE TABLE Victim_equiped_with_safety_equipment(
+    victim_id int not null references Victims(id),
+    safety_equipment_id char(1) not null references Safety_equipment(id),
+    PRIMARY KEY(victim_id, safety_equipment_id)
+);
+
+
 CREATE TABLE Victims
 (
     id       int     not null,
@@ -68,17 +82,12 @@ CREATE TABLE Victims
     victim_sex char(1),
 --- referenced ids--
     party_id int not null,
-    victim_degree_of_injury_id int not null,
-    victim_seating_position_id char(1),
-    victim_role_id int not null,
-    victim_ejected_id int,
+    victim_degree_of_injury_id int not null references Victim_degree_of_injury(id),
+    victim_seating_position_id char(1) references Victim_seating_position(id),
+    victim_role_id int not null references Victim_role(id),
+    victim_ejected_id int references Victim_ejected(id),
 --     party_id int not null REFERENCES PARTICIPANT (party_id),
-    PRIMARY KEY  (id),
-    FOREIGN KEY (victim_degree_of_injury_id) references Victim_degree_of_injury(id),
-    FOREIGN KEY (victim_seating_position_id) references Victim_seating_position(id),
-    FOREIGN KEY (victim_role_id) references Victim_role(id),
-    FOREIGN KEY (victim_ejected_id) references Victim_ejected(id)
-
+    PRIMARY KEY  (id)
 );
 ------------------------------------------------------------------------
 ------------------------------------------------------------------------
@@ -87,11 +96,110 @@ CREATE TABLE Victims
 ------------------------------------------------------------------------
 
 
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+----------------------------------Parties start-------------------------
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+
+-- Related entities with party: one to many
+CREATE TABLE Movement_preceding_collision
+(
+    id char(1) not null,
+    definition varchar(150),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Party_drug_physical
+(
+    id char(1) not null,
+    definition varchar(150),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Party_sobriety
+(
+    id char(1) not null,
+    definition varchar(150),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Party_type(
+    id int not null,
+    definition varchar(150),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE Statewide_vehicle_type(
+    id char(1) not null,
+    definition varchar(150),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE Cellphone_use(
+    id char(1) not null,
+    definition varchar(150),
+    PRIMARY KEY(id)
+);
 
 
+-- Relations with party: Many to many
+CREATE TABLE Other_associated_factor
+(
+    id char(1) not null,
+    definition varchar(150),
+    PRIMARY KEY (id)
+);
 
+CREATE TABLE Party_equiped_with_safety_equipment(
+    party_id int not null references Parties(id),
+    safety_equipment_id char(1) not null references Safety_equipment(id),
+    PRIMARY KEY(party_id, safety_equipment_id)
+);
 
+CREATE TABLE Party_associated_with_safety_other_associated_factor(
+    party_id int not null references Parties(id),
+    other_associated_factor_id char(1) not null references Other_associated_factor(id),
+    PRIMARY KEY(party_id, safety_equipment_id)
+);
 
+CREATE TABLE Financial_responsibility
+(
+    id char(1) not null,
+    definition varchar(150),
+    PRIMARY KEY (id)
+);
+
+-- Parties
+CREATE TABLE Parties(
+    id int not null,
+    -- Atributes
+    hazardous_materials char(1),
+    party_age int,
+    party_sex char(1),
+    -- relation to collision
+    collision_id int not null references Collisions(id),,
+    financial_responsibility_id char(1) references Financial_responsibility(id),
+    school_bus_related char(1) not null,
+    at_fault char(1) not null,
+    -- referenced ids
+    movement_preceding_collision_id char(1) references Movement_preceding_collision_id(id),
+    party_drug_physical_id char(1) references Party_drug_physical(id),
+    party_sobriety_id char(1) references Party_sobriety(id),
+    party_type_id int references Party_type(id),
+    statewide_vehicle_type_id char(1) references Statewide_vehicle_type(id),
+    vehicle_make varchar(150),
+    vehicle_year int,
+    cellphone_use_id char(1) Cellphone_use(id),
+    -- key
+    PRIMARY KEY(id)
+);
+
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+----------------------------------Parties end-------------------------
+------------------------------------------------------------------------
+------------------------------------------------------------------------
 
 
 
