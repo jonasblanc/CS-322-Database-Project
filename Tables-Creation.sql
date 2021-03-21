e00333afd1040cc55621f659ae6af10df6b460cd
Zad2 <56636548+Zad2@users.noreply.github.com>
2021-03-21 16:23:20 +0100

    Format Tables-Creation

--- Tables-Creation.sql
+++ Tables-Creation.sql
@@ -26,26 +26,26 @@
 
 CREATE TABLE Type_of_collision
 (
-    id char(1), --check char between a & h
+    id         char(1), --check char between a & h
     definition varchar(150),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE Collision_severity
 (
-    id int not null CHECK (0<= id and id <= 4),
+    id         int not null CHECK (0 <= id and id <= 4),
     definition varchar(150),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE Collisions
 (
-    case_id int not null,
-    collision_date date,
-    collision_time timestamp,
-    tow_away char(1) CHECK (tow_away='Y' or tow_away='N'),
-    type_of_collision_id char(1) references Type_of_collision(id),
-    collision_severity_id int not null references Collision_severity(id),
+    case_id               int not null,
+    collision_date        date,
+    collision_time        timestamp,
+    tow_away              char(1) CHECK (tow_away = 'Y' or tow_away = 'N'),
+    type_of_collision_id  char(1) references Type_of_collision (id),
+    collision_severity_id int not null references Collision_severity (id),
     PRIMARY KEY (case_id)
 );
 
@@ -56,10 +56,9 @@ CREATE TABLE Collisions
 ------------------------------------------------------------------------
 
 
-
 CREATE TABLE Safety_equipment
 (
-    id char(1) not null,
+    id         char(1) not null,
     definition varchar(150),
     PRIMARY KEY (id)
 );
@@ -71,52 +70,53 @@ CREATE TABLE Safety_equipment
 ------------------------------------------------------------------------
 CREATE TABLE Victim_degree_of_injury
 (
-    id int not null CHECK (0<= id and id<=7), -- can we make sure id and def are consistent
+    id         int not null CHECK (0 <= id and id <= 7), -- can we make sure id and def are consistent
     definition varchar(150),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE Victim_seating_position
 (
-    id char(1) , --can we check if id is number or char?
+    id         char(1), --can we check if id is number or char?
     definition varchar(150),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE Victim_role
 (
-    id int not null CHECK (1<= id and id<=6),
+    id         int not null CHECK (1 <= id and id <= 6),
     definition varchar(150),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE Victim_ejected
 (
-    id int CHECK (0<= id and id<=3), --make sure entity is still created if id is null
+    id         int CHECK (0 <= id and id <= 3), --make sure entity is still created if id is null
     definition varchar(150),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE Victims
 (
-    id       int     not null,
-    pregnant char(1) not null,
-    victim_age int,
-    victim_sex char(1),
+    id                         int     not null,
+    pregnant                   char(1) not null,
+    victim_age                 int,
+    victim_sex                 char(1),
 --- referenced ids--
-    party_id int not null,
-    victim_degree_of_injury_id int not null references Victim_degree_of_injury(id),
-    victim_seating_position_id char(1) references Victim_seating_position(id),
-    victim_role_id int not null references Victim_role(id),
-    victim_ejected_id int references Victim_ejected(id),
+    party_id                   int     not null,
+    victim_degree_of_injury_id int     not null references Victim_degree_of_injury (id),
+    victim_seating_position_id char(1) references Victim_seating_position (id),
+    victim_role_id             int     not null references Victim_role (id),
+    victim_ejected_id          int references Victim_ejected (id),
 --     party_id int not null REFERENCES PARTICIPANT (party_id),
-    PRIMARY KEY  (id)
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE Victim_equiped_with_safety_equipment(
-    victim_id int not null references Victims(id),
-    safety_equipment_id char(1) not null references Safety_equipment(id),
-    PRIMARY KEY(victim_id, safety_equipment_id)
+CREATE TABLE Victim_equiped_with_safety_equipment
+(
+    victim_id           int     not null references Victims (id),
+    safety_equipment_id char(1) not null references Safety_equipment (id),
+    PRIMARY KEY (victim_id, safety_equipment_id)
 );
 ------------------------------------------------------------------------
 ------------------------------------------------------------------------
@@ -134,108 +134,106 @@ CREATE TABLE Victim_equiped_with_safety_equipment(
 -- Related entities with party: one to many
 CREATE TABLE Movement_preceding_collision
 (
-    id char(1) not null,
+    id         char(1) not null,
     definition varchar(150),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE Party_drug_physical
 (
-    id char(1) not null,
+    id         char(1) not null,
     definition varchar(150),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE Party_sobriety
 (
-    id char(1) not null,
+    id         char(1) not null,
     definition varchar(150),
     PRIMARY KEY (id)
 );
 
-CREATE TABLE Party_type(
-    id int not null,
+CREATE TABLE Party_type
+(
+    id         int not null,
     definition varchar(150),
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE Statewide_vehicle_type(
-    id char(1) not null,
+CREATE TABLE Statewide_vehicle_type
+(
+    id         char(1) not null,
     definition varchar(150),
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE Cellphone_use(
-    id char(1) not null,
+CREATE TABLE Cellphone_use
+(
+    id         char(1) not null,
     definition varchar(150),
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
 
 
 -- Relations with party: Many to many
 CREATE TABLE Other_associated_factor
 (
-    id char(1) not null,
+    id         char(1) not null,
     definition varchar(150),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE Financial_responsibility
 (
-    id char(1) not null,
+    id         char(1) not null,
     definition varchar(150),
     PRIMARY KEY (id)
 );
 
 -- Parties
-CREATE TABLE Parties(
-    id int not null,
+CREATE TABLE Parties
+(
+    id                              int     not null,
     -- Atributes
-    hazardous_materials char(1),
-    party_age int,
-    party_sex char(1),
+    hazardous_materials             char(1),
+    party_age                       int,
+    party_sex                       char(1),
     -- relation to collision
-    collision_case_id int not null references Collisions(case_id),
-    financial_responsibility_id char(1) references Financial_responsibility(id),
-    school_bus_related char(1) not null,
-    at_fault char(1) not null,
+    collision_case_id               int     not null references Collisions (case_id),
+    financial_responsibility_id     char(1) references Financial_responsibility (id),
+    school_bus_related              char(1) not null,
+    at_fault                        char(1) not null,
     -- referenced ids
-    movement_preceding_collision_id char(1) references Movement_preceding_collision(id),
-    party_drug_physical_id char(1) references Party_drug_physical(id),
-    party_sobriety_id char(1) references Party_sobriety(id),
-    party_type_id int references Party_type(id),
-    statewide_vehicle_type_id char(1) references Statewide_vehicle_type(id),
-    vehicle_make varchar(150),
-    vehicle_year int,
-    cellphone_use_id char(1) references Cellphone_use(id),
+    movement_preceding_collision_id char(1) references Movement_preceding_collision (id),
+    party_drug_physical_id          char(1) references Party_drug_physical (id),
+    party_sobriety_id               char(1) references Party_sobriety (id),
+    party_type_id                   int references Party_type (id),
+    statewide_vehicle_type_id       char(1) references Statewide_vehicle_type (id),
+    vehicle_make                    varchar(150),
+    vehicle_year                    int,
+    cellphone_use_id                char(1) references Cellphone_use (id),
     -- key
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE Party_equiped_with_safety_equipment(
-    party_id int not null references Parties(id),
-    safety_equipment_id char(1) not null references Safety_equipment(id),
-    PRIMARY KEY(party_id, safety_equipment_id)
+CREATE TABLE Party_equiped_with_safety_equipment
+(
+    party_id            int     not null references Parties (id),
+    safety_equipment_id char(1) not null references Safety_equipment (id),
+    PRIMARY KEY (party_id, safety_equipment_id)
 );
 
-CREATE TABLE Party_associated_with_safety_other_associated_factor(
-    party_id int not null references Parties(id),
-    other_associated_factor_id char(1) not null references Other_associated_factor(id),
-    PRIMARY KEY(party_id, other_associated_factor_id)
+CREATE TABLE Party_associated_with_safety_other_associated_factor
+(
+    party_id                   int     not null references Parties (id),
+    other_associated_factor_id char(1) not null references Other_associated_factor (id),
+    PRIMARY KEY (party_id, other_associated_factor_id)
 );
 ------------------------------------------------------------------------
 ------------------------------------------------------------------------
 ----------------------------------Parties end-------------------------
 ------------------------------------------------------------------------
 ------------------------------------------------------------------------
-
-
-
-
-
-
-
-
 
 
 -- NOTE: 2 ways to create a relation
