4cfe31001e00efd3e22fba1ad9081168ed02e074
Zad2 <56636548+Zad2@users.noreply.github.com>
2021-03-21 13:04:41 +0100

    Update Table-Creations.sql
    Add a few conventions like bool, name of tables etc...

--- Tables-Creation.sql
+++ Tables-Creation.sql
@@ -1,37 +1,44 @@
--- Don't worry about the tables that are commented out
+---------------------Design implementations---------------------
+-- Boolean => char(1)
+-- definition => varchar(150)
+-- Table_name (First letter upper case then underscores)
+-- One-to-Many (Store key in one)
+-- No state is null, set key to null
+-- In an entity: id is id of current entity, create new attribute table_id for referenced id
 
--- CREATE TABLE Party
--- (
---     id                 int NOT NULL,
---     cellphone_use      char(1),
---     hazardous_material char(1),
---     PRIMARY KEY (id)
---
--- );
+--Questions
 
--- CREATE TABLE Participant
--- (
---     party_id  INT NOT NULL,
---     party_age INT,
---     party_sex char(1),
---     PRIMARY KEY (party_id),
---     FOREIGN KEY (party_id) REFERENCES Party (id) ON DELETE CASCADE
--- );
+--victim age/ pregnancy: age of 999 implies that person is not yet born, so that we don't lose information about the age of the mother
+-- there would be 2 distinct victims (mother normal age, and yet to be born child age 99)
 
-CREATE TABLE Participant
+--in Victims: attribute victim_seating_position_id || seating_position_id
+
+-- drop table VICTIMS
+-- /
+-- drop table VICTIM_DEGREE_OF_INJURY
+-- /
+
+CREATE TABLE Victim_degree_of_injury
 (
-    party_id  INT NOT NULL,
-    party_age INT,
-    party_sex char(1),
-    PRIMARY KEY (party_id)
+    id int not null CHECK (0<= id and id<=7), -- can we make sure id and def are consistent
+    definition varchar(150),
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE Victim
+CREATE TABLE Victims
 (
-    id       INT     NOT NULL,
-    pregnant char(1) NOT NULL, --or varchar(q0) depends if we want to store as Y/N or yes no
-    party_id INT     NOT NULL REFERENCES PARTICIPANT (party_id),
-    PRIMARY KEY (party_id, id)
+    id       int     not null,
+    pregnant char(1) not null,
+    victim_age int,
+    victim_sex char(1),
+--- referenced ids--
+    party_id int not null,
+    victim_degree_of_injury int not null,
+--     victim_seating_position_id char(1) ,
+--     party_id int not null REFERENCES PARTICIPANT (party_id),
+    PRIMARY KEY  (id),
+    FOREIGN KEY (victim_degree_of_injury) references Victim_degree_of_injury(id)
+--     FOREIGN KEY (victim_seating_position_id) references Victim_seating_position(id) on DELETE CASCADE
 );
 
 -- NOTE: 2 ways to create a relation
@@ -43,49 +50,12 @@ CREATE TABLE Victim
 -- E.g 1: Victim_seating_position (c.f. https://launchschool.com/books/sql/read/table_relationships one to many paragraph)
 -- Create a single table for entity and link using references
 
-
-
-CREATE TABLE Victim_Ejected
-(
-    id         int,
-    definition varchar(20),
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE Was_Ejected
-(
-    ejected_id int,
-    party_id   int not null,-- references Victim (party_id),
-    victim_id  int not null, -- references Victim (id),
-    PRIMARY KEY (party_id, victim_id),
-    FOREIGN KEY (ejected_id) references Victim_Ejected (id),
-    FOREIGN KEY (party_id, victim_id) references Victim(party_id, id)
-);
-
-CREATE TABLE Victim_Role
-(
-    id         int,
-    definition varchar(50),
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE Has_Role
-(
-    role_id int,
-    party_id   int not null,
-    victim_id  int not null,
-    PRIMARY KEY (party_id, victim_id),
-    FOREIGN KEY (role_id) references Victim_Role (id),
-    FOREIGN KEY (party_id, victim_id) references Victim(party_id, id)
-);
-
-CREATE TABLE Victim_Seating_Position
-(
-    id         char(1),
-    definition varchar(50),
-    party_id   int not null,
-    victim_id  int not null,
-    PRIMARY KEY (id),
-    FOREIGN KEY (party_id, victim_id) references Victim(party_id, id) ON DELETE CASCADE
-
-);
+-- CREATE TABLE Victim_Seating_Position
+-- (
+--     id         char(1),
+--     definition varchar(50),
+--     victim_id  int not null,
+--     PRIMARY KEY (id),
+--     FOREIGN KEY (victim_id) references Victim(id) ON DELETE CASCADE
+--
+-- );
