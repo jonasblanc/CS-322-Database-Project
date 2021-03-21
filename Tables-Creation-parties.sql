18952108523325a4c110d825cd2cc923e5c89d8b
Jonas Blanc <jonas.blanc@epfl.ch>
2021-03-21 15:44:05 +0100

    Add parties and attributes (which can be entities)

--- Tables-Creation-parties.sql
+++ Tables-Creation-parties.sql
@@ -1,56 +1,76 @@
+-- Related entities with party: one to many
 CREATE TABLE Movement_preceding_collision
 (
     id char(1) not null,
     definition varchar(150),
-    PRIMARY KEY (id),
-);
-
-CREATE TABLE Other_associated_factor
-(
-    id char(1) not null,
-    definition varchar(150),
-    PRIMARY KEY (id),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE Party_drug_physical
 (
     id char(1) not null,
     definition varchar(150),
-    PRIMARY KEY (id),
-);
-
-CREATE TABLE Safety_equipment
-(
-    id char(1) not null,
-    definition varchar(150),
-    PRIMARY KEY (id),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE Party_sobriety
 (
     id char(1) not null,
     definition varchar(150),
-    PRIMARY KEY (id),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE Party_type(
     id int not null,
     definition varchar(150),
-    PRIMARY KEY(id),
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE Statewide_vehicle_type(
     id char(1) not null,
     definition varchar(150),
-    PRIMARY KEY(id),
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE Cellphone_use(
     id char(1) not null,
     definition varchar(150),
-    PRIMARY KEY(id),
+    PRIMARY KEY(id)
 );
 
+
+-- Relations with party: Many to many
+CREATE TABLE Safety_equipment
+(
+    id char(1) not null,
+    definition varchar(150),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Other_associated_factor
+(
+    id char(1) not null,
+    definition varchar(150),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Party_equiped_with_safety_equipment(
+    party_id int not null,
+    safety_equipment_id char(1) not null,
+    PRIMARY KEY(party_id, safety_equipment_id),
+    FOREIGN KEY (party_id) references Parties(id),
+    FOREIGN KEY (safety_equipment_id) references Safety_equipment(id)
+);
+
+CREATE TABLE Party_associated_with_safety_other_associated_factor(
+    party_id int not null,
+    other_associated_factor_id char(1) not null,
+    PRIMARY KEY(party_id, safety_equipment_id),
+    FOREIGN KEY (party_id) references Parties(id),
+    FOREIGN KEY (other_associated_factor_id) references Other_associated_factor(id)
+);
+
+-- Parties
 CREATE TABLE Parties(
     id int not null,
     -- Atributes
@@ -73,5 +93,5 @@ CREATE TABLE Parties(
     FOREIGN KEY (party_sobriety_id) references Party_sobriety(id),
     FOREIGN KEY (party_type_id) references Party_type(id),
     FOREIGN KEY (statewide_vehicle_type_id) references Statewide_vehicle_type(id),
-    FOREIGN KEY (cellphone_use_id) references Cellphone_use(id),
+    FOREIGN KEY (cellphone_use_id) references Cellphone_use(id)
 );
